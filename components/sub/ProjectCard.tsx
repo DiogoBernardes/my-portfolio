5a912d2a1ee13156883af1c4acf181f942fea4ff
Diogo Bernardes <Diogobernardes@ipvc.pt>
2024-05-06 23:18:13 +0100

    feat: some changes

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -12,7 +12,7 @@ const ProjectCard = ({ src, title, description, link }: Props) => {
     <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
       <div className="flex flex-col sm:flex-row w-full z-[20]">
         <a href={link}>
-          <div className="w-full sm:w-[400px] h-[375px]">
+          <div className="w-full sm:w-[400px] h-[400px]">
             <div className="h-2/3 sm:h-2/3 relative">
               <div className="relative w-full h-full">
                 <img
@@ -22,9 +22,11 @@ const ProjectCard = ({ src, title, description, link }: Props) => {
                 />
               </div>
             </div>
-            <div className="h-1/3 sm:h-1/3 p-4">
+            <div className="h-1/3 lg:w-[380px]  sm:h-1/3 p-4">
               <h1 className="text-xl font-semibold text-white">{title}</h1>
-              <p className="mt-2 text-gray-300">{description}</p>
+              <p className="mt-2 text-gray-300 sm:whitespace-normal whitespace-pre-line truncate">
+                {description}
+              </p>
             </div>
           </div>
         </a>
